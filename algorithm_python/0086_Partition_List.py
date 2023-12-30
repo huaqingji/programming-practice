207e9f1924d7dc3440bbf0186cba064717734978
username <useremail@gmail.com>
2023-12-30 17:02:04 -0500

    add daily practice

---
+++ algorithm_python/0086_Partition_List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+
+        dummy_left, dummy_right = ListNode(), ListNode()
+
+        left, right = dummy_left, dummy_right
+        while head:
+            if head.val < x:
+                left.next = head
+                left = left.next
+            else:
+                right.next = head
+                right = right.next
+            head = head.next
+
+        right.next, left.next = None, dummy_right.next
+        return dummy_left.next
