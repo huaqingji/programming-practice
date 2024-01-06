dfcc74a3230bffb8d1fef38c0f4c3a0200a86834
username <useremail@gmail.com>
2024-01-05 20:42:08 -0500

    add daily practice

---
+++ algorithm_python/0111_Minimum_Depth_Of_Binary_Tree.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        
+        if not root:
+            return 0
+
+        queue, depth = deque([root]), 1
+        while queue:
+            length = len(queue)
+            for _ in range(length):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                if not node.left and not node.right:
+                    return depth
+            depth += 1
